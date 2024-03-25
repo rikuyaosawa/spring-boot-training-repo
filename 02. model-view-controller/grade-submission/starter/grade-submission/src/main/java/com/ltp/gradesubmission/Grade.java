bd66683a493a53d77ccf95793e5b6194bff90827
Osawa Rikuya <rikuyaosawa@gmail.com>
2024-03-25 14:30:39 +0900

    connect mvc

---
+++ 02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/Grade.java
@@ -0,0 +1,40 @@
+package com.ltp.gradesubmission;
+
+public class Grade {
+
+	private String student;
+	private String subject;
+	private String score;
+
+
+	public Grade(String student, String subject, String score) {
+		this.student = student;
+		this.subject = subject;
+		this.score = score;
+	}
+
+	public String getStudent() {
+		return this.student;
+	}
+
+	public void setStudent(String student) {
+		this.student = student;
+	}
+
+	public String getSubject() {
+		return this.subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getScore() {
+		return this.score;
+	}
+
+	public void setScore(String score) {
+		this.score = score;
+	}
+
+}
--- 02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ 02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -1,14 +1,17 @@
 package com.ltp.gradesubmission;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GradeController {
 
-	@GetMapping
-	public String sayHello() {
-		return "somehtmlview";
+	@GetMapping("/grades")
+	public String getGrades(Model model) {
+		Grade grade = new Grade("Harry", "Potions", "C-");
+		model.addAttribute("grade", grade);
+		return "grades";
 	}
 
 }
