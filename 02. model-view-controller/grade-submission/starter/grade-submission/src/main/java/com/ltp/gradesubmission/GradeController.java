d1681d3d6f81c1525bbf6376e5234275a952a663
rikuyaosawa <rikuyaosawa@gmail.com>
2024-04-10 06:35:51 +0900

    complete th:each

--- 02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ 02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -1,5 +1,7 @@
 package com.ltp.gradesubmission;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +9,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class GradeController {
 
+	ArrayList<Grade> studentGrades = new ArrayList<>();
+
 	@GetMapping("/grades")
 	public String getGrades(Model model) {
-		Grade grade = new Grade("Harry", "Potions", "C-");
-		model.addAttribute("grade", grade);
+		studentGrades.add(new Grade("Hermionie", "Arithmancy", "A+"));
+		studentGrades.add(new Grade("Rikuya", "Potions", "C+"));
+		studentGrades.add(new Grade("Harry", "Potions", "C-"));
+		model.addAttribute("grades", studentGrades);
 		return "grades";
 	}
 
