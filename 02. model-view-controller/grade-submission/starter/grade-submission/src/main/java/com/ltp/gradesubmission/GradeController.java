953e780dbb350e831acedc5717e8178a2eae2ae0
rikuyaosawa <rikuyaosawa@gmail.com>
2024-04-13 18:27:31 +0900

    complete thyme loop lesson

--- 02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ 02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -1,6 +1,7 @@
 package com.ltp.gradesubmission;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,13 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class GradeController {
 
-	ArrayList<Grade> studentGrades = new ArrayList<>();
+	List<Grade> studentGrades = Arrays.asList(
+		new Grade("Hermionie", "Arithmancy", "A+"),
+		new Grade("Rikuya", "Potions", "C+"),
+		new Grade("Harry", "Potions", "C-")
+	);
 
 	@GetMapping("/grades")
 	public String getGrades(Model model) {
-		studentGrades.add(new Grade("Hermionie", "Arithmancy", "A+"));
-		studentGrades.add(new Grade("Rikuya", "Potions", "C+"));
-		studentGrades.add(new Grade("Harry", "Potions", "C-"));
 		model.addAttribute("grades", studentGrades);
 		return "grades";
 	}
